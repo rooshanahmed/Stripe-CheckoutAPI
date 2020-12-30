a4905040d2ed6af6e343c7c99fd529c17aba55e7
Rooshan Ahmed <rooshan.ahmed@hotmail.com>
2020-12-30 23:26:13 +0500

    CheckoutForm Created

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,11 +1,22 @@
 import React from "react"
-import { CardElement } from "@stripe/react-stripe-js"
+import { CardElement, useStripe } from "@stripe/react-stripe-js"
 import "./CheckoutForm.css"
 
 export default function CheckoutForm() {
+  const stripe = useStripe();
+  
+  const submitCheckout = async() => {
+      const response = await fetch('/.netlify/functions/checkout', { method: "post" });
+      const data = await response.json();
+      console.log('Data = ',data);
+  }
+  
   return (
     <div>
       <CardElement />
+      <div>
+          <button onClick={submitCheckout}>Checkout</button>
+      </div>
     </div>
   )
 }
