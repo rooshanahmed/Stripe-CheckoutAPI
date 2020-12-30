7bf0d1260487a3add19418ec5bc3efdee183f6b3
Rooshan Ahmed <rooshan.ahmed@hotmail.com>
2020-12-30 21:59:29 +0500

    Stripe Integrated In Server

--- functions/checkout/checkout.js
+++ functions/checkout/checkout.js
@@ -1,14 +1,16 @@
-const stripe = require('stripe')();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const handler = async (event) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: 'usd',
+    metadata: { integration_check: 'accept_a_payment' },
+  })
+
   try {
-    const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ client_secret: paymentIntent.client_secret }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
