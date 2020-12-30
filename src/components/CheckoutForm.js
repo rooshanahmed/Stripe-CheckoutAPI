c0db7e1be50bd4e8b0b506c2d4db14f5e5dcc511
Rooshan Ahmed <rooshan.ahmed@hotmail.com>
2020-12-30 23:34:15 +0500

    CheckoutForm.js Updated

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,21 +1,51 @@
 import React from "react"
-import { CardElement, useStripe } from "@stripe/react-stripe-js"
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
 import "./CheckoutForm.css"
 
+const CARD_ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      color: "#32325d",
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: "antialiased",
+      fontSize: "16px",
+      "::placeholder": {
+        color: "#aab7c4",
+      },
+    },
+    invalid: {
+      color: "#fa755a",
+      iconColor: "#fa755a",
+    },
+  },
+}
+
 export default function CheckoutForm() {
-  const stripe = useStripe();
-  
-  const submitCheckout = async() => {
-      const response = await fetch('/.netlify/functions/checkout', { method: "post" });
-      const data = await response.json();
-      console.log('Data = ',data);
+  const stripe = useStripe()
+  const elements = useElements()
+
+  const submitCheckout = async () => {
+    const response = await fetch("/.netlify/functions/checkout", {
+      method: "post",
+    })
+    const data = await response.json()
+    console.log("Data = ", data)
+
+    const result = await stripe.confirmCardPayment(data.client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Rooshan Ahmed",
+        },
+      },
+    })
   }
-  
+
   return (
     <div>
-      <CardElement />
+      <CardElement options={CARD_ELEMENT_OPTIONS} />
       <div>
-          <button onClick={submitCheckout}>Checkout</button>
+        <button onClick={submitCheckout}>Checkout</button>
       </div>
     </div>
   )
